99313e9b1251980b29c19dba82339aff30b6eb29
Ross Boast <33289376+rossboast@users.noreply.github.com>
2025-12-12 10:00:55 +0000

    Centralize BASE_HREF configuration and fix index.html base href replacement

--- packages/website/scripts/update-env.sh
+++ packages/website/scripts/update-env.sh
@@ -21,6 +21,7 @@ base_href="${BASE_HREF:-/}"
 perl -i -pe "s/__VERSION__/$version/g" $dist_folder/main.*.js
 perl -i -pe "s/__AI_SEARCH_URL__/$ai_search_url/g" $dist_folder/main.*.js
 perl -i -pe "s|__BASE_HREF__|$base_href|g" $dist_folder/404.html
+perl -i -pe "s|<base href=\"/\">|<base href=\"$base_href\">|g" $dist_folder/index.html
 
 echo Version: $version
 echo Base href: $base_href
